d380f29329258948aca02df4f27d42c4117e7530
kamaljami5833-dotcom <kamaljami5833@gmail.com>
2026-09-21 14:25:43 +0530

    Add files via upload

---
+++ p1.java
@@ -0,0 +1,13 @@
+class Programeone{
+  
+            static int a;
+            static float b;
+            static short c;
+            static boolean d;
+            static char e;
+            static long f;
+            static double g;
+    public static void main(String[] args){
+    System.out.println(a+" "+b+" "+c+" "+d+" "+e+" "+f+" "+g);
+}
+}
