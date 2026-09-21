088fa9f17810ffbf9040a3eedea57221997c6ab6
kamaljami5833-dotcom <kamaljami5833@gmail.com>
2026-09-21 15:08:52 +0530

    Refactor quadratic root calculation into findRoots method
    Refactored the main method to use a separate findRoots method for calculating and displaying the roots of a quadratic equation.

--- Quadratic.java
+++ Quadratic.java
@@ -1,33 +1,27 @@
 import java.util.Scanner;
 public class Quadratic{
-
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the value of a:");
-        int a = sc.nextInt();
-        System.out.println("Enter the value of b:");
-        int b = sc.nextInt();
-        System.out.println("Enter the value of c:");
-        int c = sc.nextInt();
-        
-        double discriminant = b * b - 4 * a * c;
-        
-        if(discriminant > 0){
-            double root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
-            double root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
-            System.out.println("Roots are real and different.");
-            System.out.println("Root 1: " + root1);
-            System.out.println("Root 2: " + root2);
-        }
-        else if(discriminant == 0)
-            {
-                double root = -b / (2 * a);
-                System.out.println("Roots are real and same.");
-                System.out.println("Root: " + root);
-            } 
-        else 
-            {
-               System.out.println("Roots are complex and different.");
-            }
+static void findRoots(int a, int b, int c) {
+    double discriminant = b * b - 4 * a * c;
+    if (discriminant > 0) {
+        double root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+        double root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+        System.out.println("Roots are real and different.");
+        System.out.println("Root 1: " + root1);
+        System.out.println("Root 2: " + root2);
+    } else if (discriminant == 0) {
+        double root = -b / (2 * a);
+        System.out.println("Roots are real and the same.");
+        System.out.println("Root: " + root);
+    } else {
+        System.out.println("Roots are complex and different.");
+    }
+}
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter coefficients a, b and c: ");
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    int c = sc.nextInt();
+        findRoots(a, b, c);
     }
 }
